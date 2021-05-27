e2e30898a8e79d2bd8297c8e65d91a89699ec638
Sham E Ansari <saprince93@gmail.com>
2021-05-27 13:47:56 +0600

    fixing nav bar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,18 +1,28 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import {Link} from 'react-router-dom'
-
+import {userContext} from '../App'
 const Navbar = () => {
+    const {state,dispatch} = useContext(userContext)
+    const renderList=()=>{
+        if(state){
+            return [
+                <li><Link to="/profile">Profile</Link></li>,
+                <li><Link to="/createpost">CreatePost</Link></li>
+            ]
+        }else {
+            return [
+                <li><Link to="/signin">Sign in</Link></li>,
+                <li><Link to="/signup">Sign Up</Link></li>
+            ]
+        }
+    }
     return(
         <nav>
             <div className="nav-wrapper white">
                 <div className="navigation">
-                    <Link to="/" className="brand-logo left">Meme Verse</Link>
+                    <Link to={state?"/":"/signin"} className="brand-logo left">Meme Verse</Link>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
-                        <li><Link to="/signin">Sign in</Link></li>
-                        <li><Link to="/signup">Sign Up</Link></li>
-                        <li><Link to="/profile">Profile</Link></li>
-                        <li><Link to="/createpost">CreatePost</Link></li>
-                        
+                        {renderList()}
                     </ul>
                 </div>
             </div>
